00f297e36f6bf737154f5faa02d8703f436c116e
pslasso <paul.lasso.91@gmail.com>
2022-05-10 17:54:17 +0200

    Add env port

--- server.js
+++ server.js
@@ -29,11 +29,7 @@ app.use(express.static('website'));
 
 // Setup Server
 const port = 8000;
-const server = app.listen(port, listening);
-
-function listening() {
-    console.log(`runing on localhost: ${port}`);
-};
+app.listen(process.env.PORT || port, () => console.log(`Server listening on port${port}`)); 
 // TODO-ROUTES!
 //GET
 

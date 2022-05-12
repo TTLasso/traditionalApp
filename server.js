884488f612112cebf42113fc98edb142f9390566
pslasso <paul.lasso.91@gmail.com>
2022-05-12 23:04:33 +0200

    coneccion a DB mysql

--- server.js
+++ server.js
@@ -4,11 +4,22 @@ projectData = {};
 // Require Express to run server and routes
 
 const express = require('express');
+const mysql = require('mysql');
+
+//Coneccion
+
+const connection = mysql.createConnection({
+    host: 'us-cdbr-east-05.cleardb.net',
+    user: 'b4859299f642d6',
+    password: 'd73e64fb',
+    database: 'heroku_45a5f34f29adc01'
+})
 
 
 // Start up an instance of app
 
 const app = express();
+
 
 /* Middleware*/
 
@@ -29,7 +40,48 @@ app.use(express.static('website'));
 
 // Setup Server
 const port = 8000;
-app.listen(process.env.PORT || port, () => console.log(`Server listening on port${port}`)); 
+app.listen(process.env.PORT || port, () => console.log(`Server listening on port ${port}`));
+
+//Motor 
+// app.set('view engine', 'ejs');
+
+
+// var sql = `INSERT INTO ingresos (id, ciudad, sentimiento) VALUES ('2' , 'manta' , 'asdasdasd' , NOW())`;
+// connection.query(sql, function (error, rows) {
+//     if (error) throw error;
+
+//     if (!error) {
+//         console.log(rows)
+//     }
+// })
+
+// app.post('/registro', function (req, res) {
+//     var date = req.body.newDate;
+//     var temp = req.body.temp;
+//     var feeling = req.body.feelings;
+
+//     connection.query(`INSERT INTO ingresos (id, ciudad, sentimiento) SET ?`, temp, feeling, feeling, function (error, results, fields) {
+//         if (error) throw error;
+//         res.end(JSON.stringify(results));
+//     });
+// });
+
+
+connection.query('SELECT * FROM ingresos WHERE id = "1"', (error, rows) => {
+    if (error) throw error;
+
+    if (!error) {
+        console.log(rows)
+    }
+})
+
+
+// //Renderizar pagina principal
+// app.get('/', function (req, res) {
+//     res.render('index.ejs')
+// })
+
+
 // TODO-ROUTES!
 //GET
 
@@ -55,6 +107,13 @@ function addTemp(req, res) {
     data.push(newEntrie)
     res.send(data)
     console.log(data)
+
+    var date = req.body.newDate;
+    var temp = req.body.weather;
+    var feeling = req.body.feelings;
+    connection.query(`INSERT INTO ingresos (id, ciudad, sentimiento) VALUES ("6","${feeling}","${temp}")`);
+
 }
+
 
 
